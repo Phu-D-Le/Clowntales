b1266e1b678f10ec8f5491f9bbb5a75d1cfa597e
Phu Le <phudangle0506@gmail.com>
2023-02-04 11:14:00 -0600

    Added WIP Player Controller

---
+++ Project Clowntales/Assets/_Game/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    public void movePlayer()
+    {
+        if (move.sqrMagnitude > 0.1f) { 
+            Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
