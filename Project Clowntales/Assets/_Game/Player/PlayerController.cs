ae9e3b816ba70fb03a894e9b6ea333cd5e666f5a
Phu Le <phudangle0506@gmail.com>
2023-02-04 21:48:40 -0600

    Bernardadded basic Player Controller with mouse look and controller support

--- Project Clowntales/Assets/_Game/Player/PlayerController.cs
+++ Project Clowntales/Assets/_Game/Player/PlayerController.cs
@@ -6,31 +6,111 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed;
-    private Vector2 move;
+    private Vector2 move, mouseLook, joystickLook;
+    private InputActionReference mouseAim;
+    private Vector3 rotationTarget;
+    private CharacterController characterController;
+    public bool lookAtMouse = false;
+
+    public void OnMouseAim(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            print("Attack");
+            lookAtMouse = true;
+        }
+        else
+        {
+            lookAtMouse = false;
+        }
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
+    }
+    public void OnJoystickLook(InputAction.CallbackContext context)
+    {
+        joystickLook = context.ReadValue<Vector2>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        if (lookAtMouse == true) // Mouse and Keyboard detected
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+
+            moveWhileAim();
+        }
+
+        else // Controller detected
+        {
+            if(joystickLook.x == 0 && joystickLook.y == 0)
+            {
+                movePlayer();
+            }
+            else
+            {
+                moveWhileAim();
+            }
+        }
     }
 
     public void movePlayer()
     {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
         if (move.sqrMagnitude > 0.1f) { 
-            Vector3 movement = new Vector3(move.x, 0f, move.y);
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
         }
+
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
+    }
+    public void moveWhileAim()
+    {
+        if (lookAtMouse)
+        {
+            var lookPos = rotationTarget - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+        }
+
+        else
+        {
+            Vector3 aimDirection = new Vector3(joystickLook.x, 0f, joystickLook.y);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
     }
 }
